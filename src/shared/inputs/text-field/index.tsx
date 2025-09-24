3a77036a06fb7b47715a10f13dc87fe92ab91858
Ronak Kapadi <ronakk@itpathsolutions.com>
2025-09-24 13:14:22 +0530

    Add: visible password content for type password input

--- src/shared/inputs/text-field/index.tsx
+++ src/shared/inputs/text-field/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TextField } from '@mui/material';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { MdVisibilityOff, MdVisibility } from 'react-icons/md';
 
 const CustomInput = ({
   placeholder,
@@ -10,13 +11,41 @@ const CustomInput = ({
   error,
   ...props
 }: any) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+
   return (
     <TextField
       {...{ placeholder, name, value, error }}
+      {...props}
       fullWidth={true}
       onChange={handleChange}
       onBlur={handleBlur}
-      {...props}
+      slotProps={{
+        input: {
+          ...(props?.type === 'password' ? {
+            endAdornment: <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  edge="start"
+                >
+                  {showPassword ? <MdVisibilityOff /> : <MdVisibility />}
+                </IconButton>
+            </InputAdornment>
+          } : { endAdornment: props?.endAdornment || null }),
+        }
+      }}
+      type={
+        props?.type === 'password'
+          ? showPassword
+            ? 'text'
+            : 'password'
+          : props?.type
+      }
     />
   );
 };
