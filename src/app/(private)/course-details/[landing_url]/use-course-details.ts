d2dea6eba7a87e4a6a2509a38bbfba6fab2b2ee6
Krima Shah <krimas.itpath@gmail.com>
2025-10-17 12:18:12 +0530

    ENHANCE: Add condition to check total duration before lesson progression logic in useCourseDetails

--- src/app/(private)/course-details/[landing_url]/use-course-details.ts
+++ src/app/(private)/course-details/[landing_url]/use-course-details.ts
@@ -1475,6 +1475,7 @@ const useCourseDetails = ({
 
         const isLessonCompleted = cloneCurrentLession?.user_watch_time_histories?.[0]?.is_completed;
 
+        if (cloneCurrentLession?.total_duration) {
             if (cloneCurrentLession?.type === 'video' && !isLessonCompleted) {
                 const payload = {
                     course_id: getUserCourseProgressData?.id,
@@ -1608,6 +1609,7 @@ const useCourseDetails = ({
             handleModuleWiseChangeCourse(
                 getUserCourseProgressApiDataForCopy?.course_translations?.[0]?.course_modules
             );
+        }
     }, [
         selectedLesson,
         getUserCourseProgressData,
